ef02507fca126bfaa9acd487d2dc29394ae07660
YazanAhmad-Eng <54546895+YazanAhmad-Eng@users.noreply.github.com>
2020-04-22 01:47:39 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo apt install python
-sudo apt install gunicorn
+sudo apt install python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools
+
 pip install --user --upgrade pip
